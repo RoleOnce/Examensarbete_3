e35de3f946d99f61e8bf7ded94215f43f01c7fbb
RoleOnce <malcolmuzuriaga@hotmail.com>
2024-11-12 10:06:33 +0100

    Add: Register user page

--- src/main/java/org/roleonce/examensarbete_3/controller/UserController.java
+++ src/main/java/org/roleonce/examensarbete_3/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.roleonce.examensarbete_3.controller;
 
+import org.roleonce.examensarbete_3.model.CustomUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +12,13 @@ public class UserController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/register")
+    public String registerUser(Model model) {
+
+        model.addAttribute("customUser", new CustomUser());
+
+        return "register";
+    }
+
 }
