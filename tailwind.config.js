eb329fbea6c3ee3e7cb9ef807420b4c2a0eed7a1
RoleOnce <malcolmuzuriaga@hotmail.com>
2024-12-21 22:54:28 +0100

    Add: Styling on Homepage and Listingpage with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/main/resources/templates/**/*.html",  // För Thymeleaf templates
+    "./src/main/resources/static/**/*.{html,js}" // För statiska filer
+  ],
   theme: {
     extend: {},
   },
